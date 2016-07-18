1f86aa4c21eea26a4fe53a62e580efb5bee765a1
Eugene Batalov <eabatalov89@gmail.com>
2016-07-18 21:10:56 +0300

    gc: implement link remaps deletion
    We reuse minimal subset of criu restore command logic sufficient
    to locate all the link remaps in all ps tree mnt namespaces.
    No ps tree processes are created but all the ps tree mnt
    namespaces and mountpoints are created. After creation of ps
    tree mnt namespaces each link remap is deleted in its namespace.
    
    Signed-off-by: Eugene Batalov <eabatalov89@gmail.com>

--- criu/include/files-reg.h
+++ criu/include/files-reg.h
@@ -52,6 +52,7 @@ extern void delete_link_remaps(void);
 extern void free_link_remaps(void);
 extern int prepare_remaps(void);
 extern void try_clean_remaps(void);
+extern int gc_link_remaps(void);
 
 extern int strip_deleted(struct fd_link *link);
 
