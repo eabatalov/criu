8b2f3ee9890e1bd0e3326013191c75ff64bd0682
Mike Rapoport <rppt@linux.vnet.ibm.com>
2016-12-15 14:10:38 +0200

    lazy-pages: stop checking for zero pagemaps
    A page that explicitly mapped to zero pfn or a page that is not present
    should be treated in the same way, therefore the zero pagemaps are not
    required and will be removed by the following commits.
    
    travis-ci: success for revert zero pagemaps
    Signed-off-by: Mike Rapoport <rppt@linux.vnet.ibm.com>
    Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>

--- criu/uffd.c
+++ criu/uffd.c
@@ -587,8 +587,8 @@ static int uffd_seek_or_zero_pages(struct lazy_pages_info *lpi, __u64 address,
 
 	lpi->pr.reset(&lpi->pr);
 
-	ret = lpi->pr.seek_pagemap(&lpi->pr, address, false);
-	if (!ret || pagemap_zero(lpi->pr.pe))
+	ret = lpi->pr.seek_pagemap(&lpi->pr, address, true);
+	if (!ret)
 		return uffd_zero(lpi, address, nr);
 
 	lpi->pr.skip_pages(&lpi->pr, address - lpi->pr.pe->vaddr);
